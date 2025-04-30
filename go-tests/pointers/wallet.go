6a03fac2293b4df1b140ecc396dc60e1db30e3ff
yashasn <yashasn14598@gmail.com>
2025-04-30 22:40:24 +0200

    inlcuded errors

--- go-tests/pointers/wallet.go
+++ go-tests/pointers/wallet.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 /*
 In Go if a symbol (variables, types, functions et al) starts with a lowercase symbol then
@@ -9,11 +12,6 @@ it is private outside the package it's defined in
 
 // Go lets you create new types from existing ones. type MyName OriginalType
 type Bitcoin int
-
-type Wallet struct {
-	//since it is lowercase it is private
-	balance Bitcoin
-}
 
 type Stringer interface {
 	String() string
@@ -24,6 +22,11 @@ func (b Bitcoin) String() string {
 	return fmt.Sprintf("%d BTC", b)
 }
 
+type Wallet struct {
+	//since it is lowercase it is private
+	balance Bitcoin
+}
+
 /* This FAILS if func (w Wallet) is used!! Why?
 When calling func (w Wallet) Deposit(amount int) the w is a copy of whatever we called the method from.
 */
@@ -31,6 +34,19 @@ When calling func (w Wallet) Deposit(amount int) the w is a copy of whatever we 
 func (w *Wallet) Deposit(amount Bitcoin) {
 	fmt.Printf("address of balance in Deposit is %p \n", &w.balance)
 	w.balance += amount
+}
+
+//NOTE : The var keyword allows us to define values global to the package. Here, we can use this error during test assertion
+
+var ErrInsufficientFunds = errors.New("You are broke!!")
+
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+
+	if amount > w.balance {
+		return ErrInsufficientFunds
+	}
+	w.balance -= amount
+	return nil
 }
 
 func (w *Wallet) Balance() Bitcoin {
--- go-tests/pointers/wallet_test.go
+++ go-tests/pointers/wallet_test.go
@@ -6,14 +6,55 @@ import (
 )
 
 func TestWallet(t *testing.T) {
-	wallet := Wallet{}
-	//You don’t need to manually write &wallet.Deposit(10) — Go implicitly handles that
-	wallet.Deposit(10)
-	got := wallet.Balance()
-	fmt.Printf("address of balance in test is %p \n", &wallet.balance)
 
-	want := Bitcoin(0)
-	if got != want {
-		t.Errorf("got %s want %s", got, want)
+	assertBalance := func(t testing.TB, wallet Wallet, want Bitcoin) {
+		t.Helper()
+		got := wallet.Balance()
+
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
 	}
+
+	assertError := func(t testing.TB, got error, want string) {
+		t.Helper()
+		if got == nil {
+			//fatal stops ends the test and stops further assertions
+			t.Fatal("wanted an error but didn't get one")
+		}
+
+		if got.Error() != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	}
+
+	t.Run("deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		//You don’t need to manually write &wallet.Deposit(10) — Go implicitly handles that
+		wallet.Deposit(Bitcoin(10))
+		fmt.Printf("address of balance in test is %p \n", &wallet.balance)
+
+		want := Bitcoin(10)
+		assertBalance(t, wallet, want)
+	})
+
+	t.Run("withdraw", func(t *testing.T) {
+		//able to set balance because it is set as private within the same package
+		wallet := Wallet{balance: Bitcoin(20)}
+		//You don’t need to manually write &wallet.Deposit(10) — Go implicitly handles that
+		wallet.Withdraw(Bitcoin(10))
+		assertBalance(t, wallet, Bitcoin(10))
+	})
+
+	t.Run("withdraw insufficient funds", func(t *testing.T) {
+		//able to set balance because it is set as private within the same package
+		wallet := Wallet{balance: Bitcoin(20)}
+		//You don’t need to manually write &wallet.Deposit(10) — Go implicitly handles that
+		err := wallet.Withdraw(Bitcoin(30))
+
+		assertError(t, err, ErrInsufficientFunds.Error())
+		//assertBalance(t, wallet, Bitcoin(10))
+
+	})
+
 }
