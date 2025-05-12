c5ada060e50290b089871d6e8a794bc6b2f2c641
yashasn <yashasn14598@gmail.com>
2025-05-12 18:04:52 +0200

    more of contexts

--- go-tests/context/context.go
+++ go-tests/context/context.go
@@ -7,10 +7,36 @@ import (
 
 type Store interface {
 	Fetch() string
+	Cancel()
 }
 
 func Server(store Store) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, store.Fetch())
+		ctx := r.Context()
+
+		/*
+			The channel can hold one string value without requiring a receiver to be ready.
+			Once one value is in the channel, any further sends (data <- "value") will block until the value is received.
+			Example :
+			data := make(chan string, 1)
+
+			data <- "hello" // success, buffer has space
+
+			data <- "world" // blocks here, buffer is full and no receiver
+		*/
+
+		data := make(chan string, 1)
+
+		go func() {
+			data <- store.Fetch()
+		}()
+
+		select {
+		case d := <-data:
+			fmt.Fprint(w, d)
+		case <-ctx.Done():
+			store.Cancel()
+		}
+
 	}
 }
--- go-tests/context/context_test.go
+++ go-tests/context/context_test.go
@@ -1,31 +1,64 @@
 package context
 
 import (
+	"context"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 )
 
 type SpyStore struct {
-	response string
+	response  string
+	cancelled bool
 }
 
 func (s *SpyStore) Fetch() string {
+	time.Sleep(10 * time.Millisecond)
 	return s.response
 }
 
+func (s *SpyStore) Cancel() {
+	s.cancelled = true
+}
+
 func TestServer(t *testing.T) {
-	//why &SpyStore and not just SpyStore, because Fetch() have a pointer receiver and not value receiver. So only *SpyStore is considered to implement Fetch
-	//So only *SpyStore implments the interface and not SpyStore
-	data := "Hello, World"
-	spyStore := &SpyStore{response: data}
-	server := Server(spyStore)
 
-	request := httptest.NewRequest(http.MethodGet, "/", nil)
-	response := httptest.NewRecorder()
+	t.Run("positive case", func(t *testing.T) {
+		//why &SpyStore and not just SpyStore, because Fetch() have a pointer receiver and not value receiver. So only *SpyStore is considered to implement Fetch
+		//So only *SpyStore implments the interface and not SpyStore
+		data := "Hello, World"
+		spyStore := &SpyStore{response: data}
+		server := Server(spyStore)
 
-	server.ServeHTTP(response, request)
-	if response.Body.String() != data {
-		t.Errorf(`got "%s", want "%s"`, response.Body.String(), data)
-	}
+		request := httptest.NewRequest(http.MethodGet, "/", nil)
+		response := httptest.NewRecorder()
+
+		server.ServeHTTP(response, request)
+		if response.Body.String() != data {
+			t.Errorf(`got "%s", want "%s"`, response.Body.String(), data)
+		}
+	})
+
+	t.Run("store should cancel work if req is cancelled", func(t *testing.T) {
+
+		data := "Hello, World"
+		spyStore := &SpyStore{response: data}
+		server := Server(spyStore)
+
+		request := httptest.NewRequest(http.MethodGet, "/", nil)
+
+		cancellingCtx, cancel := context.WithCancel(request.Context())
+		time.AfterFunc(5*time.Millisecond, cancel)
+		request = request.WithContext(cancellingCtx)
+
+		response := httptest.NewRecorder()
+
+		server.ServeHTTP(response, request)
+
+		if !spyStore.cancelled {
+			t.Error("store was not told to cancel")
+		}
+
+	})
 }
